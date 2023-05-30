552d4efd46be24db56538657001166f5fd66c8d9
gdsa006 <gdsa006@gmail.com>
2023-05-30 21:38:06 +0530

    changes

--- project/frontend/src/components/pages/PostAd.js
+++ project/frontend/src/components/pages/PostAd.js
@@ -8,11 +8,21 @@ function PostAd() {
 
     return (
         <div>
-            <section className={`${postad.mySection}`}>
+            <section className={`${postad.mySection} ${postad.postAd}`}>
+                <div className={`${postad.backgroundContainer}`}></div>
+                <div className={`${postad.backgroundContainer} ${postad.backgroundContainer2}`}></div>
+                <div className={`${postad.backgroundContainer} ${postad.backgroundContainer3}`}></div>
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-6">
-                            text
+                            <h1>Something extraordinary<br />that is something <span>unique</span></h1>
+                            <h5>Line to target customers</h5>
+                            <p>
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                            </p>
+                            <p>
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                            </p>
                         </div>
                         <div class="col-lg-6">
                             <AdForm />
--- project/frontend/src/components/partials/AdForm.js
+++ project/frontend/src/components/partials/AdForm.js
@@ -1,128 +1,196 @@
-import React from 'react';
-import { Card, Form, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, Form, Button, Row, Col } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
+import { useDropzone } from 'react-dropzone';
+import adform from './AdForm.module.css';
 
 function AdForm() {
-  const { register, handleSubmit } = useForm();
+    const { register, handleSubmit } = useForm();
 
-  const onSubmit = (data) => {
-    // Handle form submission
-    console.log(data);
-  };
+    const onSubmit = (data) => {
+        // Handle form submission
+        console.log(data);
+    };
 
-  return (
-    <Card className="shadow">
-      <Card.Body>
-      <Form onSubmit={handleSubmit(onSubmit)} className="form-spacing">
-          <Form.Group controlId="adTitle">
-            <Form.Label>Ad Title</Form.Label>
-            <Form.Control type="text" {...register("adTitle", { required: true })} />
+    const [uploadedImages, setUploadedImages] = useState([]);
+
+    const handleImageDrop = (acceptedFiles) => {
+        setUploadedImages((prevImages) => [...prevImages, ...acceptedFiles]);
+    };
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop: handleImageDrop, multiple: true });
+
+
+    return (
+        <Card className={`${adform.shadow} ${adform.card}`}>
+            <Card.Body>
+                <Form onSubmit={handleSubmit(onSubmit)} className="form-spacing">
+                    <Form.Group className='mb-3' controlId="adTitle">
+                        <Form.Label>Ad Title</Form.Label>
+                        <Form.Control type="text" {...register("adTitle", { required: true })} />
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId="offerSeeker">
+                        <Form.Label>Select an option</Form.Label>
+                        <div className="radio-inline">
+                            <Form.Check
+                                inline
+                                type="radio"
+                                name="offerSeeker"
+                                label="Offer"
+                                value="offer"
+                                {...register("offerSeeker", { required: true })}
+                            />
+                            <Form.Check
+                                inline
+                                type="radio"
+                                name="offerSeeker"
+                                label="Seeker"
+                                value="seeker"
+                                {...register("offerSeeker", { required: true })}
+                            />
+                        </div>
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId="b2bB2c">
+                        <div className="radio-inline">
+                            <Form.Check
+                                inline
+                                type="radio"
+                                name="b2bB2c"
+                                label="B2B"
+                                value="b2b"
+                                {...register("b2bB2c", { required: true })}
+                            />
+                            <Form.Check
+                                inline
+                                type="radio"
+                                name="b2bB2c"
+                                label="B2C"
+                                value="b2c"
+                                {...register("b2bB2c", { required: true })}
+                            />
+                        </div>
+                    </Form.Group>
+
+                    <Row>
+                        <Col>
+                            <Form.Group className='mb-3' controlId="category">
+                                <Form.Label>Category</Form.Label>
+                                <Form.Control as="select" {...register("category", { required: true })}>
+                                    <option value="">Select Category</option>
+                                    <option value="category1">Category 1</option>
+                                    <option value="category2">Category 2</option>
+                                    <option value="category3">Category 3</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group controlId="subCategory">
+                                <Form.Label>Sub Category</Form.Label>
+                                <Form.Control as="select" {...register("subCategory", { required: true })}>
+                                    <option value="">Select Sub Category</option>
+                                    <option value="subCategory1">Sub Category 1</option>
+                                    <option value="subCategory2">Sub Category 2</option>
+                                    <option value="subCategory3">Sub Category 3</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+
+
+                    <Form.Group className='mb-3' controlId="adImages">
+                        <Form.Label>Ad Images</Form.Label>
+                        <div {...getRootProps()} className={`${adform.dropzone} ${isDragActive ? 'active' : ''}`}>
+                            <input {...getInputProps()} />
+                            {isDragActive ? (
+                                <p>Drop the images here...</p>
+                            ) : (
+                                <p>Drag and drop images here, or click to select files</p>
+                            )}
+                        </div>
+                        {uploadedImages.length > 0 && (
+                            <div>
+                                <p>Uploaded Images:</p>
+                                {uploadedImages.map((file, index) => (
+                                    <img key={index} src={URL.createObjectURL(file)} alt={`uploaded-image-${index}`} className={`${adform.uploadedImage}`} />
+                                ))}
+                            </div>
+                        )}
+                    </Form.Group>
+
+
+                    <Form.Group className='mb-3' controlId="adDescription">
+                        <Form.Label>Ad Description</Form.Label>
+                        <Form.Control as="textarea" rows={3} {...register("adDescription", { required: true })} />
+                    </Form.Group>
+
+
+                    <Form.Group className='mb-3' controlId="barterExchange">
+            <Form.Label>Barter/Exchange</Form.Label>
+            <Row>
+                        <Col>
+                            <Form.Group controlId="category">
+                                <Form.Label>Category</Form.Label>
+                                <Form.Control as="select" {...register("category", { required: true })}>
+                                    <option value="">Select Category</option>
+                                    <option value="category1">Category 1</option>
+                                    <option value="category2">Category 2</option>
+                                    <option value="category3">Category 3</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group controlId="subCategory">
+                                <Form.Label>Sub Category</Form.Label>
+                                <Form.Control as="select" {...register("subCategory", { required: true })}>
+                                    <option value="">Select Sub Category</option>
+                                    <option value="subCategory1">Sub Category 1</option>
+                                    <option value="subCategory2">Sub Category 2</option>
+                                    <option value="subCategory3">Sub Category 3</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </Col>
+                    </Row>
           </Form.Group>
 
-          <Form.Group controlId="offerSeeker">
-            <Form.Label>Select an option</Form.Label>
-            <div className="radio-inline">
-              <Form.Check
-                inline
-                type="radio"
-                name="offerSeeker"
-                label="Offer"
-                value="offer"
-                {...register("offerSeeker", { required: true })}
-              />
-              <Form.Check
-                inline
-                type="radio"
-                name="offerSeeker"
-                label="Seeker"
-                value="seeker"
-                {...register("offerSeeker", { required: true })}
-              />
-            </div>
-          </Form.Group>
+                    <Form.Group className='mb-3' controlId="postedBy">
+                        <Form.Label>Posted By</Form.Label>
+                        <div className="radio-inline">
+                            <Form.Check
+                                inline
+                                type="radio"
+                                name="postedBy"
+                                label="Owner"
+                                value="owner"
+                                {...register("postedBy", { required: true })}
+                            />
+                            <Form.Check
+                                inline
+                                type="radio"
+                                name="postedBy"
+                                label="Agent"
+                                value="agent"
+                                {...register("postedBy", { required: true })}
+                            />
+                            <Form.Check
+                                inline
+                                type="radio"
+                                name="postedBy"
+                                label="Broker"
+                                value="broker"
+                                {...register("postedBy", { required: true })}
+                            />
+                        </div>
+                    </Form.Group>
 
-          <Form.Group controlId="b2bB2c">
-            <Form.Label>Select an option</Form.Label>
-            <div className="radio-inline">
-              <Form.Check
-                inline
-                type="radio"
-                name="b2bB2c"
-                label="B2B"
-                value="b2b"
-                {...register("b2bB2c", { required: true })}
-              />
-              <Form.Check
-                inline
-                type="radio"
-                name="b2bB2c"
-                label="B2C"
-                value="b2c"
-                {...register("b2bB2c", { required: true })}
-              />
-            </div>
-          </Form.Group>
-
-          <Form.Group controlId="category">
-            <Form.Label>Category</Form.Label>
-            <Form.Control as="select" {...register("category", { required: true })}>
-              <option value="">Select Category</option>
-              <option value="category1">Category 1</option>
-              <option value="category2">Category 2</option>
-              <option value="category3">Category 3</option>
-            </Form.Control>
-          </Form.Group>
-
-          <Form.Group controlId="subCategory">
-            <Form.Label>Sub Category</Form.Label>
-            <Form.Control as="select" {...register("subCategory", { required: true })}>
-              <option value="">Select Sub Category</option>
-              <option value="subCategory1">Sub Category 1</option>
-              <option value="subCategory2">Sub Category 2</option>
-              <option value="subCategory3">Sub Category 3</option>
-            </Form.Control>
-          </Form.Group>
-
-          <Form.Group controlId="adDescription">
-            <Form.Label>Ad Description</Form.Label>
-            <Form.Control as="textarea" rows={3} {...register("adDescription", { required: true })} />
-          </Form.Group>
-
-          <Form.Group controlId="postedBy">
-            <Form.Label>Posted By</Form.Label>
-            <div className="radio-inline">
-            <Form.Check
-            inline
-              type="radio"
-              name="postedBy"
-              label="Owner"
-              value="owner"
-              {...register("postedBy", { required: true })}
-            />
-            <Form.Check
-            inline
-              type="radio"
-              name="postedBy"
-              label="Agent"
-              value="agent"
-              {...register("postedBy", { required: true })}
-            />
-            <Form.Check
-            inline
-              type="radio"
-              name="postedBy"
-              label="Broker"
-              value="broker"
-              {...register("postedBy", { required: true })}
-            />
-            </div>
-          </Form.Group>
-
-          <Button variant="primary" type="submit">Submit</Button>
-        </Form>
-      </Card.Body>
-    </Card>
-  );
+                    <div className="text-center">
+                        <Button variant="primary" type="submit" style={{ width: "200px" }}>Accept & Post</Button>
+                    </div>
+                </Form>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default AdForm;
