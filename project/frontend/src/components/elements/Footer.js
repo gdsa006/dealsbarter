98d30be4cf030b91f094378f449a7b6c1b1d728d
gdsa006 <gdsa006@gmail.com>
2023-06-06 19:51:03 +0530

    changes

--- project/frontend/src/components/elements/Footer.js
+++ project/frontend/src/components/elements/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import footerStyles from './Footer.module.css';
 
 const Footer = () => {
@@ -14,40 +14,29 @@ const Footer = () => {
             <h4>About Us</h4>
             <p>dealsBarter.com portal is created with the vision of helping people who are involved in barter deals but find it difficult to find suitable deals for themselves nearby &amp; over web.</p>
             <div className={footerStyles.socialIcons}>
-              <FontAwesomeIcon icon={faFacebookF} />
-              <FontAwesomeIcon icon={faTwitter} />
-              <FontAwesomeIcon icon={faInstagram} />
+              <a href="https://www.facebook.com/profile.php?id=100092649663821" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faFacebookF} />
+              </a>
+              <a href="https://twitter.com/barter_deals" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+              <a href="https://www.instagram.com/dealsbarter/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
+              <a href="https://www.youtube.com/channel/UC6OVubPy4Z0wCzTAHKdDN2w" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faYoutube} />
+              </a>
             </div>
           </Col>
           <Col md={4}>
             <h4>Contact Us</h4>
-            <div className={footerStyles.contactInfo}>
-              <div className={footerStyles.icon}>
-                <FontAwesomeIcon icon={faMapMarkerAlt} />
-              </div>
-              <div>
-                <p>123 Street, City</p>
-                <p>State, Country</p>
-              </div>
-            </div>
-            <div className={footerStyles.contactInfo}>
-              <div className={footerStyles.icon}>
-                <FontAwesomeIcon icon={faEnvelope} />
-              </div>
-              <div>
-                <p>info@example.com</p>
-                <p>support@example.com</p>
-              </div>
-            </div>
-            <div className={footerStyles.contactInfo}>
-              <div className={footerStyles.icon}>
-                <FontAwesomeIcon icon={faPhone} />
-              </div>
-              <div>
-                <p>+1234567890</p>
-                <p>+0987654321</p>
-              </div>
-            </div>
+            <ul className={footerStyles.quickLinks}>
+              <li><a href="/">Home</a></li>
+              <li><a href="/about-us/">About Us</a></li>
+              <li><a href="/contact-us/">Contact Us</a></li>
+              <li><a href="#properties">Menu</a></li>
+              <li><a href="#contact">Menu</a></li>
+            </ul>
           </Col>
           <Col md={4}>
             <h4>Quick Links</h4>
--- project/frontend/src/components/elements/Showcase.js
+++ project/frontend/src/components/elements/Showcase.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import showcase from './Showcase.module.css';
-import image1 from '../../images/wideImage.jpg'; // Import your first image
-import image2 from '../../images/wideImage.jpg'; // Import your second image
+import image1 from '../../images/7473709.jpg'; // Import your first image
+import image2 from '../../images/proservices.jpg'; // Import your second image
 import image3 from '../../images/wideImage.jpg'; // Import your third image
 import CategoryImage from '../../images/pexels-photo-1547248.webp'; // Import your logo image
-
 function Showcase() {
   return (
     <section className={`section d-none, ${showcase.mySection}`}>
@@ -14,12 +13,13 @@ function Showcase() {
             <a href="#" className={showcase.clickableColumn}>
               <div className={showcase.subColumn}>
                 <div className={showcase.imageContainer}>
-                  <img src={CategoryImage} alt="Image 1" className={showcase.image} />
+                  <img src={image2} alt="Image 1" className={showcase.image} />
                 </div>
                 <div className={showcase.textContainer}>
                   <h3 className={showcase.heading}>Professional Services</h3>
                   <p className={showcase.description}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Explore professional services e.g. CA, Accountant, IT Consultants, Marketing &amp; Creative team,
+                    Advertising, Architect etc. exchanging their Services with Other Service, Coupon or Products
                   </p>
                 </div>
               </div>
@@ -30,12 +30,13 @@ function Showcase() {
             <a href="#" className={showcase.clickableColumn}>
               <div className={showcase.subColumn}>
                 <div className={showcase.imageContainer}>
-                  <img src={CategoryImage} alt="Image 2" className={showcase.image} />
+                  <img src={image1} alt="Image 2" className={showcase.image} />
                 </div>
                 <div className={showcase.textContainer}>
                   <h3 className={showcase.heading}>Products</h3>
                   <p className={showcase.description}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Explore Products e.g. Electronic, Furniture, Household, Automobiles, Fashion, Property, Fitness &amp;
+                    Sports, Commercial &amp; Industrial, Books in Barter / Exchange with other Service, Coupon or Products
                   </p>
                 </div>
               </div>
@@ -51,7 +52,8 @@ function Showcase() {
                 <div className={showcase.textContainer}>
                   <h3 className={showcase.heading}>Health and Wellness</h3>
                   <p className={showcase.description}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Explore opportunity for exchange in Fitness, Wellness, Salon, Gym, Salon, Health in Barter /
+                    Exchange with other Service, Coupon or Products
                   </p>
                 </div>
               </div>
@@ -67,7 +69,9 @@ function Showcase() {
                 <div className={showcase.textContainer}>
                   <h3 className={showcase.heading}>Jobs</h3>
                   <p className={showcase.description}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Explore Jobs, training posted directly by Company HR for Office, Field, WFH, IT, Hotel &amp; travel,
+                    Education, Household, Call Centre Jobs or Services related to HR Consultant in Barter / Exchange for
+                    free
                   </p>
                 </div>
               </div>
@@ -84,7 +88,8 @@ function Showcase() {
                 <div className={showcase.textContainer}>
                   <h3 className={showcase.heading}>Property</h3>
                   <p className={showcase.description}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Explore property services or properties of all over India exchange with other property, business
+                    share, Joint Venture or any other form of Barter / Exchange.
                   </p>
                 </div>
               </div>
@@ -100,13 +105,13 @@ function Showcase() {
                 <div className={showcase.textContainer}>
                   <h3 className={showcase.heading}>Automobiles</h3>
                   <p className={showcase.description}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Explore Automobile Services all over India exchange with other Service or Products.
                   </p>
                 </div>
               </div>
             </a>
           </div>
-          
+
           {/* Repeat the above structure for the remaining columns */}
 
         </div>
